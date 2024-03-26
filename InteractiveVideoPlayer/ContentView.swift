0e861638b1aa573d1bc792f01135c14668c3a731
Enrique Aliaga <e.aliaga.chavez@gmail.com>
2024-03-26 11:36:31 -0500

    Initial Commit

---
+++ InteractiveVideoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  InteractiveVideoPlayer
+//
+//  Created by Enrique Aliaga on 26/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ InteractiveVideoPlayer/InteractiveVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  InteractiveVideoPlayerApp.swift
+//  InteractiveVideoPlayer
+//
+//  Created by Enrique Aliaga on 26/03/24.
+//
+
+import SwiftUI
+
+@main
+struct InteractiveVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
